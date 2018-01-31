5baf5bcf35bdd91d2122ca49731ae57ebc603cd9
Arpita <arpitajain0811@gmail.com>
2018-01-31 15:35:45 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+   "extends": "airbnb-base",
+
+    "env": {
+      "jest" : true
+    },
+
+    "rules": {
+      "no-throw-literal" :"off",
+           "no-console" : "off",
+           "no-debugger":"off",
+           "consistent-return":"off"
+       }
+};
