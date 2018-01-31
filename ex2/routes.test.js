3f8dc9197e13b190ccd2509b2fc37729bc7924c0
Arpita <arpitajain0811@gmail.com>
2018-01-31 21:05:43 +0530

    added test cases for ex2

---
+++ ex2/routes.test.js
@@ -0,0 +1,23 @@
+const routes = require('./routes.js');
+const request = require('request');
+
+describe('creates a server that listens on the input port', () => {
+  it('returns Hello name when an http request is sent with name as path parameter', (done) => {
+    const call = (data) => {
+      request(data, (error, response, body) => {
+        expect(body).toEqual('Hello arpita');
+        done();
+      });
+    };
+    routes('arpita', call);
+  });
+  it('returns Hello when an http request is sent with no path parameter', (done) => {
+    const call = (data) => {
+      request(data, (error, response, body) => {
+        expect(body).toEqual('Hello');
+        done();
+      });
+    };
+    routes('/', call);
+  });
+});
