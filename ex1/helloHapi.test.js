a68a05da5d423896eac73a9984a049ccb37498e4
Arpita <arpitajain0811@gmail.com>
2018-01-31 18:02:59 +0530

    installed request dependency and modified test for ex1

--- ex1/helloHapi.test.js
+++ ex1/helloHapi.test.js
@@ -1,10 +1,13 @@
 const helloHapi = require('./helloHapi.js');
+const request = require('request');
 
 describe('creates a server that listens on the input port', () => {
   it('returns Hello hapi when an http request is sent', (done) => {
     const call = (data) => {
-      expect(data).toEqual('http://localhost:8080');
-      done();
+      request(data, (error, response, body) => {
+        expect(body).toEqual('Hello hapi');
+        done();
+      });
     };
     helloHapi(call);
   });
