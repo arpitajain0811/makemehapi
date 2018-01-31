704ceb82f44f5bf53859c6a7a4a406d7231cab05
Arpita <arpitajain0811@gmail.com>
2018-01-31 22:38:11 +0530

    code for routes.js to pass the first test case

---
+++ ex2/routes.js
@@ -0,0 +1,22 @@
+const Hapi = require('hapi');
+
+const routes = (name, callback) => {
+  const server = new Hapi.Server();
+
+  server.connection({
+    host: 'localhost',
+    port: 8000,
+    // port: Number(process.argv[2] || 8080),
+  });
+  server.route({
+    path: '/{name}',
+    method: 'GET',
+    handler: (request, reply) => reply(`Hello ${request.params.name}`),
+  // reply();,
+  });
+  server.start(() => {
+    const url = `${server.info.uri}/${name}`;
+    callback(url);
+  });
+};
+module.exports = routes;
